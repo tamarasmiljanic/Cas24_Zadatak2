240c34f1f9fa06a878f6b9f784aa49c61b3afb60
tamarasmiljanic <62512999+tamarasmiljanic@users.noreply.github.com>
2020-04-29 15:02:19 +0200

    HomeWork Assignment2 Modifications

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas24_Zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite racunsku operaciju:");
+            string op = Console.ReadLine();
+            Console.Write("Unesite prvi broj>");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Unesite drugi broj>");
+            double b = Convert.ToDouble(Console.ReadLine());
+            
+            if ((b > a) && ((op=="-") || (op=="/"))) 
+                SimpleCalculations.Swap(ref a,ref b);
+            Console.WriteLine("{0} {1} {2} = {3:0.00}", a, op, b, SimpleCalculations.Calc(op, a, b));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleCalculations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas24_Zadatak2
+{
+    class SimpleCalculations
+    {
+        public static double Calc(string op, double a, double b)
+        {
+            double r = 0;
+            switch (op)
+            {
+                case "+":
+                    r = SimpleCalculations.Add(a, b);
+                    break;
+                case "-":
+                    r = SimpleCalculations.Substract(a, b);
+                    break;
+                case "*":
+                    r = SimpleCalculations.Multiply(a, b);
+                    break;
+                case "/":
+                    r = SimpleCalculations.Divide(a, b);
+                    break;
+            }
+            return r;
+        }
+            
+    public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("!DIV0-nije dozvoljeno deljenje nulom");
+                return 0;
+            }
+        }
+        public static void Swap(ref double a, ref double b)
+        {
+            double pom;
+            pom = a;
+            a = b;
+            b = pom;
+        }
+    }
+}
